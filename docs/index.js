f52bfee04f8e9b0bd313776c015d59378061a440
aristov <vv.aristov@gmail.com>
2017-01-25 20:35:36 +0300

    dom example

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,50 @@
-import { xmlelement, xmldocument, doctype, fragment, text, cdata, comment, instruction } from '../lib'
+import {
+    fragment,
+    xmlelement as element,
+    text,
+    cdata,
+    comment,
+    instruction
+} from '../lib'
 
-const element = xmlelement('title', 'XML Document')
+const title = function(init) {
+    return element('title', init)
+}
 
-document.documentElement.append(element.node)
+const example = function(init) {
+    return element('example', init)
+}
+
+const assembler = fragment([
+    title('XML document'),
+    example([
+        title('Element'),
+        element('element', {
+            id : 'e0000001',
+            className : 'sample',
+            attributes : { key : 'value' },
+            children : 'Element node'
+        })
+    ]),
+    example([
+        title('Text'),
+        text('Text node')
+    ]),
+    example([
+        title('CDATA section'),
+        cdata('CDATA <section> node')
+    ]),
+    example([
+        title('Comment'),
+        comment('Comment node')
+    ]),
+    example([
+        title('Processing instruction'),
+        instruction({
+            target : 'example',
+            data : 'type="test"'
+        })
+    ])
+])
+
+assembler.parentNode = document.documentElement
