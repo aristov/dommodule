4d67c60e593ba2a08cff40ea209dc0a6ad5e7f78
aristov <vv.aristov@gmail.com>
2017-01-25 19:50:17 +0300

    xml examples

--- docs/index.js
+++ docs/index.js
@@ -1,50 +1,5 @@
 import { xmlelement, xmldocument, doctype, fragment, text, cdata, comment, instruction } from '../lib'
 
-const element = xmlelement('element', {
-    id : 'xmlelement',
-    className : 'example',
-    classList : ['foo', 'bar', 'wiz'],
-    children : [
-        'XML Element node',
-        fragment([
-            xmlelement('document', 'doc'),
-            xmlelement('fragment', 'frag'),
-            xmlelement('children', 'chil'),
-        ]),
-        text('Text node'),
-        cdata('<CDATA section="node"><example/></CDATA>'),
-        comment('Comment node'),
-        instruction('xml', 'Processing instruction node')
-    ].map((item, i) => [
-        i && xmlelement({
-            namespaceURI : 'http://www.w3.org/1999/xhtml',
-            qualifiedName : 'hr',
-        }),
-        item
-    ])
-})
+const element = xmlelement('title', 'XML Document')
 
 document.documentElement.append(element.node)
-
-console.log(xmlelement())
-console.log(xmldocument())
-console.log(xmldocument('root'))
-console.log(xmldocument({
-    namespaceURI : 'http://www.w3.org/1999/xhtml',
-    qualifiedName : 'html',
-    doctype : doctype('html')
-}))
-console.log(doctype('html'))
-console.log(doctype({ name : 'docx7', publicId : 'blackjack227', systemId : 'morph4' }))
-console.log(fragment([
-    xmlelement('document', 'doc'),
-    xmlelement('fragment', 'frag'),
-    xmlelement('children', 'chil'),
-]))
-console.log(text('Text node'))
-console.log(cdata('CDATA section node'))
-console.log(comment('Comment node'))
-console.log(instruction('processing', 'instruction="node"!"example"'))
-
-// CDATASection
-// PINSTRuction =)
