3a317601bcf451fd21bfaad4ef235808fade9a28
aristov <vv.aristov@gmail.com>
2017-01-25 05:53:47 +0300

    create from descriptor

--- docs/index.js
+++ docs/index.js
@@ -4,7 +4,24 @@ const element = xmlelement('element', {
     id : 'xmlelement',
     className : 'example',
     classList : ['foo', 'bar', 'wiz'],
-    children : 'XML Element node'
+    children : [
+        'XML Element node',
+        fragment([
+            xmlelement('document', 'doc'),
+            xmlelement('fragment', 'frag'),
+            xmlelement('children', 'chil'),
+        ]),
+        text('Text node'),
+        cdata('<CDATA section="node"><example/></CDATA>'),
+        comment('Comment node'),
+        instruction('xml', 'Processing instruction node')
+    ].map((item, i) => [
+        i && xmlelement({
+            namespaceURI : 'http://www.w3.org/1999/xhtml',
+            qualifiedName : 'hr',
+        }),
+        item
+    ])
 })
 
 document.documentElement.append(element.node)
@@ -18,13 +35,16 @@ console.log(xmldocument({
     doctype : doctype('html')
 }))
 console.log(doctype('html'))
-console.log(doctype({ name : 'aaa', publicId : 'bbb', systemId : 'ccc' }))
+console.log(doctype({ name : 'docx7', publicId : 'blackjack227', systemId : 'morph4' }))
 console.log(fragment([
-    xmlelement('document'),
-    xmlelement('fragment'),
-    xmlelement('children'),
+    xmlelement('document', 'doc'),
+    xmlelement('fragment', 'frag'),
+    xmlelement('children', 'chil'),
 ]))
 console.log(text('Text node'))
 console.log(cdata('CDATA section node'))
 console.log(comment('Comment node'))
-console.log(instruction('xml', 'Processing instruction node'))
+console.log(instruction('processing', 'instruction="node"!"example"'))
+
+// CDATASection
+// PINSTRuction =)
