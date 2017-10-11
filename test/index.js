396044daa0fff2e2c62bcc710d7b7ae76c5fb4dc
aristov <vv.aristov@gmail.com>
2017-10-11 15:02:30 +0300

    Improve test script

--- test/index.js
+++ test/index.js
@@ -2,21 +2,36 @@
     const dommodule = window.dommodule
 
     const {
-        attr, cdata, comment,
-        doctype, document, element,
-        fragment, instruction,
-        range, text, xml,
+        attr,
+        cdata,
+        comment,
+        doctype,
+        document,
+        element,
+        fragment,
+        instruction,
+        text,
     } = dommodule
 
-    console.log(attr())
-    console.log(attr({}))
-    console.log(attr({ namespaceURI : 'http://www.w3.org/1999/xhtml' }))
-    console.log(attr({ name : 'testname' }))
-    console.log(attr({ name : 'testname', value : 'testvalue' }))
-    console.log(attr({
-        namespaceURI : 'http://www.example.com/namespace',
-        name : 'ns-example',
-        value : 'ns-example-value'
-    }))
-    console.log(range({}))
+    element({
+        parentNode : document.documentElement,
+        attributes : [
+            attr(),
+            attr({}),
+            attr({ namespaceURI : 'http://www.w3.org/1999/xhtml' }),
+            attr({ name : 'testname' }),
+            attr({ name : 'testname', value : 'testvalue' }),
+            attr({
+                namespaceURI : 'http://www.example.com/namespace',
+                name : 'ns-example',
+                value : 'ns-example-value'
+            })
+        ],
+        childNodes : [
+            cdata('CDATASection node'),
+            comment('Comment node'),
+            instruction('PI node'),
+            text('Text node')
+        ]
+    })
 }
