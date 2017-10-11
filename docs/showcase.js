211e2f8dba768f02f86bb65aa3b49df41ad6eb08
aristov <vv.aristov@gmail.com>
2017-10-11 15:02:53 +0300

    Improve showcase example

--- docs/showcase.js
+++ docs/showcase.js
@@ -1,4 +1,5 @@
 import {
+    ElementAssembler,
     fragment,
     element,
     text,
@@ -8,43 +9,53 @@ import {
 } from '../lib'
 
 const title = function(init) {
-    return element({ qualifiedName : 'title' }).init(init)
+    return new ElementAssembler({ qualifiedName : 'title' }, init)
 }
 
 const example = function(init) {
-    return element({ qualifiedName : 'example'}).init(init)
+    return new ElementAssembler({ qualifiedName : 'example' }, init)
 }
 
-const assembler = fragment([
-    title('XML document'),
-    example([
-        title('Element'),
-        element({
-            id : 'e0000002',
-            className : 'sample',
-            attributes : { key : 'value' },
-            children : 'Element node'
-        })
-    ]),
-    example([
-        title('Text'),
-        text('Text node')
-    ]),
-    example([
-        title('CDATA section'),
-        cdata('CDATA section node')
-    ]),
-    example([
-        title('Comment'),
-        comment('Comment node')
-    ]),
-    example([
-        title('Processing instruction'),
-        instruction({
-            target : 'example',
-            data : 'ProcessingInstruction node'
-        })
-    ])
-])
+instruction({
+    parentNode : document,
+    target : 'xml-stylesheet',
+    data : { href : 'showcase.css' }
+})
 
-assembler.parentNode = document.documentElement
+element({
+    qualifiedName : 'document',
+    parentNode : document,
+    children : [
+        fragment([
+            title('XML document'),
+            example([
+                title('Element'),
+                element({
+                    id : 'e0000002',
+                    className : 'sample',
+                    attributes : { key : 'value' },
+                    children : 'Element node'
+                })
+            ]),
+            example([
+                title('Text'),
+                text('Text node')
+            ]),
+            example([
+                title('CDATA section'),
+                cdata('CDATA section node')
+            ]),
+            example([
+                title('Comment'),
+                comment('Comment node')
+            ]),
+            example([
+                title('Processing instruction'),
+                instruction({
+                    target : 'example',
+                    data : 'ProcessingInstruction node'
+                })
+            ])
+        ])
+    ]
+})
