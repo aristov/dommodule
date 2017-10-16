743c69c023a73981cf8d7665a5eb34a362350a07
aristov <vv.aristov@gmail.com>
2017-10-16 03:17:24 +0300

    Setup mocha, add first specs

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ const path = require('path')
 
 module.exports = {
     entry : {
-        element : ['./docs/element-test.js'],
-        attr : ['./docs/attr-test.js'],
-        index : ['./docs/index.js'],
-        dommodule : ['./lib'],
+        spec : ['./docs/spec.js'],
+        // element : ['./docs/element-test.js'],
+        // attr : ['./docs/attr-test.js'],
+        // index : ['./docs/index.js'],
+        // dommodule : ['./lib'],
         // 'jsdom.dommodule' : ['./lib/index'],
-        // 'window.dommodule' : ['./lib/window.dommodule']
+        // 'window.dommodule' : ['./lib/window.dommodule'],
     },
     output : {
         path : path.join(__dirname, 'dist'),
