e5f556e01994647347ff820f37e027792c1d1927
aristov <vv.aristov@gmail.com>
2017-10-16 16:24:22 +0300

    Add mocha stub

---
+++ test/mocha-stub.js
@@ -0,0 +1,20 @@
+/**
+ * @param {String} msg
+ * @param {Function} fn
+ */
+function describe(msg, fn) {}
+
+/**
+ * @param {String} msg
+ * @param {Function} fn
+ */
+function it(msg, fn) {}
+
+const assert = {}
+
+/**
+ * @param {*} act
+ * @param {*} exp
+ * @param {String} [msg]
+ */
+assert.equal = function (act, exp, msg) {}
