607bf1b4d554ae925ce3aff682449fde3e92c975
aristov <vv.aristov@gmail.com>
2017-10-18 20:19:32 +0300

    More AttrAssembler specs (wip)

--- spec/attr.spec.js
+++ spec/attr.spec.js
@@ -1,40 +1,75 @@
 import chai from 'chai'
-import { ElementAssembler, AttrAssembler } from '../lib/'
+import { AttrAssembler } from '../lib/'
 
 const { assert } = chai
-const { Element, Attr } = window
+const { Attr } = window
 
 describe('AttrAssembler', () => {
     describe('new AttrAssembler', () => {
         const instance = new AttrAssembler
         const node = instance.node
-        it('Created node has proper inheritance', () => {
+        it('instanceof Attr', () => {
             assert(node instanceof Attr, 'instanceof Attr')
         })
-        it('Created node has proper name', () => {
+        it('name', () => {
             assert.equal(node.name, 'attr')
         })
-        it('Created node has no value', () => {
+        it('value', () => {
             assert.equal(node.value, '')
-        })
-    })
-    describe('new AttrAssembler({ name, value })', () => {
-        const instance = new AttrAssembler({ name : 'foo', value : 'bar' })
-        const node = instance.node
-        it('Created node has proper name', () => {
-            assert.equal(node.name, 'foo')
-        })
-        it('Created node has proper value', () => {
-            assert.equal(node.value, 'bar')
         })
     })
     describe('new AttrAssembler(new String)', () => {
         const instance = new AttrAssembler('foobar')
         const node = instance.node
-        it('Created node has proper name', () => {
+        it('name', () => {
             assert.equal(node.name, 'attr')
         })
-        it('Created node has proper value', () => {
+        it('value', () => {
+            assert.equal(node.value, 'foobar')
+        })
+    })
+    describe('new AttrAssembler({ name, value })', () => {
+        const instance = new AttrAssembler({ name : 'foo', value : 'bar' })
+        const node = instance.node
+        it('name', () => {
+            assert.equal(node.name, 'foo')
+        })
+        it('value', () => {
+            assert.equal(node.value, 'bar')
+        })
+    })
+    describe('new AttrAssembler({ node })', () => {
+        const node = document.createAttribute('foo')
+        node.value = 'bar'
+        const instance = new AttrAssembler({ node })
+        const attr = instance.node
+        it('node equal', () => {
+            assert.equal(attr, node)
+        })
+        it('name', () => {
+            assert.equal(attr.name, 'foo')
+        })
+        it('value', () => {
+            assert.equal(attr.value, 'bar')
+        })
+    })
+    describe('new AttrAssembler({ namespaceURI, prefix, localName })', () => {
+        const instance = new AttrAssembler({
+            namespaceURI : 'http://www.w3.org/XML/1998/namespace',
+            prefix : 'xml',
+            localName : 'id',
+            value : 'foobar'
+        })
+        const node = instance.node
+        it('namespaceURI', () => {
+            assert.equal(node.namespaceURI, 'http://www.w3.org/XML/1998/namespace')
+        })
+        it('prefix, localName, name', () => {
+            assert.equal(node.prefix, 'xml')
+            assert.equal(node.localName, 'id')
+            assert.equal(node.name, 'xml:id')
+        })
+        it('value', () => {
             assert.equal(node.value, 'foobar')
         })
     })
