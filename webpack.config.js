1c649d1493d1b3b8d2a96580873ea39b03c9b67a
aristov <vv.aristov@gmail.com>
2017-10-25 16:52:01 +0300

    Configure Webpack build process: build window.dommodule.js and dist.dommodule.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,37 @@
 'use strict'
 
 const path = require('path')
-
-module.exports = {
-    entry : {
-        spec : ['./spec/index.spec.js'],
-        // document : ['./test/document-test.js'],
-        // docs : ['./test/index.js'],
-        // element : ['./test/element-test.js'],
-        // attr : ['./test/attr-test.js'],
-        // index : ['./test/index.js'],
-        // dommodule : ['./lib'],
-        'jsdom.dommodule' : ['./lib/index'],
-    },
-    output : {
-        path : path.join(__dirname, 'dist'),
-        filename : 'dist.[name].js',
-        // libraryTarget : 'commonjs2'
-    },
-    module : process.env.NODE_ENV === 'test'? {
-        loaders : [{
-            test : /\.js$/,
-            loader : 'babel-loader'
-        }]
-    } : undefined
+const babelLoader = {
+    test : /\.js$/,
+    loader : 'babel-loader'
 }
+const distPath = path.join(__dirname, 'dist')
+
+module.exports = [
+    {
+        entry : './lib/index.js',
+        output : {
+            path : distPath,
+            filename : 'dist.dommodule.js',
+            libraryTarget : 'commonjs2'
+        },
+        module : { loaders : [babelLoader] }
+    },
+    {
+        entry : './lib/index.js',
+        output : {
+            path : distPath,
+            filename : 'window.dommodule.js',
+            library : 'dommodule',
+            libraryTarget : 'window'
+        },
+        module : { loaders : [babelLoader] }
+    },
+    {
+        entry : './spec/index.spec.js',
+        output : {
+            path : path.join(__dirname, 'build'),
+            filename : 'build.spec.js'
+        }
+    }
+]
