ddad13f62eabb6555c9953df294840ca0f49df80
aristov <vv.aristov@gmail.com>
2018-05-18 03:15:31 +0300

    ElementAssembler: implement the namespace + localName resolving mechanism, add specs

--- spec/element.spec.js
+++ spec/element.spec.js
@@ -1067,6 +1067,48 @@ describe('ElementAssembler', () => {
             assert.equal(res[0].node, id5)
         })
     })
+    describe('resolveElement', () => {
+        const namespace = 'http://example.com/ns#'
+        class E1 extends ElementAssembler {}
+        E1.register()
+        class Ex extends ElementAssembler {
+            static get localName() { return this === Ex? '' : super.localName } // fixme
+            static get namespace() {
+                return namespace
+            }
+        }
+        Ex.register()
+        class E2 extends Ex {}
+        E2.register()
+        class E3 extends E2 {}
+        E3.register()
+        it('localName only', () => {
+            const element = document.createElementNS('', 'e1')
+            assert.equal(ElementAssembler.resolveElement(element), E1)
+        })
+        it('unknown localName', () => {
+            const element = document.createElementNS('', 'e0')
+            assert.equal(ElementAssembler.resolveElement(element), ElementAssembler)
+        })
+        it('namespace + localName', () => {
+            const element = document.createElementNS(namespace, 'e2')
+            assert.equal(ElementAssembler.resolveElement(element), E2)
+        })
+        it('namespace + localName', () => {
+            const element = document.createElementNS(namespace, 'e3')
+            assert.equal(ElementAssembler.resolveElement(element), E3)
+        })
+        it('namespace + unknown localName', () => {
+            const element = document.createElementNS(namespace, 'e4')
+            assert.equal(ElementAssembler.resolveElement(element), Ex)
+        })
+        it('unknown namespace + any localName', () => {
+            const element1 = document.createElementNS('http://example.org/ns#', 'e1')
+            const element2 = document.createElementNS('http://example.org/ns#', 'e0')
+            assert.equal(ElementAssembler.resolveElement(element1), ElementAssembler)
+            assert.equal(ElementAssembler.resolveElement(element2), ElementAssembler)
+        })
+    })
     /*describe('normalize', () => {
         const test = element(['text1', 'text2'])
         test.normalize()
