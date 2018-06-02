aed68685d90d0cc1da4e39bdfc0dcd67c19734a7
aristov <vv.aristov@gmail.com>
2018-06-02 19:23:36 +0300

    Update esmodule and refactor

--- test/test.js
+++ test/test.js
@@ -1,9 +1,21 @@
 const { JSDOM } = require('jsdom')
-const { window } = new JSDOM
-
-global.window = window
 global.sinon = require('sinon')
+global.window = (new JSDOM).window
+
 window.XMLSerializer = require('./serializer')
 window.Element.prototype.closest = require('./closest')
+
+/*
+ * https://github.com/jsdom/jsdom/issues/1641
+ */
+if(Object.getPrototypeOf(window.Attr) !== window.Node) {
+    Object.defineProperty(window.Attr.prototype, 'nodeType', {
+        configurable : true,
+        enumerable : true,
+        get() {
+            return window.Node.ATTRIBUTE_NODE
+        }
+    })
+}
 
 require('../spec/index.spec.js')
